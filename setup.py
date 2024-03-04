7770dea178237fd59ecf8b7204ef6d38aa3eb32a
Rio Dev <91432414+riodev99@users.noreply.github.com>
2024-03-04 07:54:24 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,31 +1,37 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
     name='pymailers',
-    version='1.1.8',
-    description='The classic email sending library for Python.',
+    version='1.2.1',
+    description='An email sending library for Python with support for HTML content and attachments.',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    author='Rio Agung Purnomo',
-    author_email='rioagungpurnomo.ak@gmail.com',
-    url='https://github.com/rioagungpurnomo/pymailers',
-    packages=['pymailers'],
+    author='riodev99',
+    author_email='jgeniya1994@gmail.com',
+    url='https://github.com/riodev99/pymailersexpanded',
+    project_urls={
+        'Source': 'https://github.com/riodev99/pymailersexpanded',  # The "Source" label URL pointing to GitHub repo
+    },
+    packages=['pymailersexpanded'],
+    install_requires=[
+        'jinja2>=2.11.3',  # Specify the minimum version of jinja2 required
+    ],
+    python_requires='>=3.6',
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
     ],
-    python_requires='>=3.6',
-    install_requires=[
-    ],
+    keywords='email smtp attachments html mail jinja2',
 )
