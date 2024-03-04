a9670c37b8daee9e833080f6170d7f10c88bed26
Rio Dev <91432414+riodev99@users.noreply.github.com>
2024-03-04 08:41:13 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='pymailers',
-    version='1.2.5',
+    version='1.3.0',
     description='An email sending library for Python with support for HTML content and attachments.',
     long_description=long_description,
     long_description_content_type="text/markdown",
