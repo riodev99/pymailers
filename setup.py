e438fd149cc1851ed8c7b513e7396302ad6aec0e
Rio Dev <91432414+riodev99@users.noreply.github.com>
2024-03-04 08:46:56 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,37 +1,39 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+des = 'An email sending library for Python with'
+dess = 'support for HTML content and attachments'
+
 setup(
     name='pymailers',
-    version='1.3.0',
-    description='An email sending library for Python with support for HTML content and attachments.',
+    version='1.5.1',
+    description=des + ' ' + dess,
     long_description=long_description,
     long_description_content_type="text/markdown",
     author='riodev99',
     author_email='jgeniya1994@gmail.com',
-    url='https://github.com/riodev99/pymailersexpanded',
+    url='https://github.com/riodev99/pymailersexpanded', 
     project_urls={
         'Source': 'https://github.com/riodev99/pymailersexpanded',  # The "Source" label URL pointing to GitHub repo
     },
     packages=['pymailers'],
-    install_requires=[
-        'jinja2>=2.11.3',  # Specify the minimum version of jinja2 required
-    ],
-    python_requires='>=3.6',
     classifiers=[
-        # Trove classifiers
-        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'Development Status :: 4 - Beta',
+        'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
-    keywords='email smtp attachments html mail jinja2',
+    python_requires='>=3.6',
+    install_requires=[
+      "requests",
+      "bs4"
+    ],
 )
